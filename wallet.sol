fb2a7ba1b374dccc7a25f112a9f77b828e474238
Hunter Long <Info@socialeck.com>
2017-08-20 21:43:55 -0600

    Update wallet.sol

--- wallet.sol
+++ wallet.sol
@@ -23,86 +23,114 @@ contract Token {
     function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
 }
 
-contract RemoteWallet {
+
+contract TradersWallet {
     
     address public owner;
     string public version;
-    etherDelta public deltaDeposits;
+    etherDelta private ethDelta;
     address public ethDeltaDepositAddress;
     
-    function RemoteWallet() {
+    // init the TradersWallet()
+    function TradersWallet() {
         owner = msg.sender;
         version = "ALPHA 0.1";
         ethDeltaDepositAddress = 0x8d12A197cB00D4747a1fe03395095ce2A5CC6819;
-        deltaDeposits = etherDelta(ethDeltaDepositAddress);
+        ethDelta = etherDelta(ethDeltaDepositAddress);
     }
     
+    // default function
     function() payable {
         
     }
     
+    // standard erc20 token balance in wallet from specific token address
     function tokenBalance(address tokenAddress) constant returns (uint) {
         Token token = Token(tokenAddress);
         return token.balanceOf(this);
     }
     
-    function transferToken(address tokenAddress, address sendTo, uint256 amount) external {
-        if (msg.sender!=owner) throw;
-        Token token = Token(tokenAddress);
-        token.transfer(sendTo, amount);
-    }
-    
+    // standard erc20 transferFrom function
     function transferFromToken(address tokenAddress, address sendTo, address sendFrom, uint256 amount) external {
-        if (msg.sender!=owner) throw;
+        require(msg.sender==owner);
         Token token = Token(tokenAddress);
         token.transferFrom(sendTo, sendFrom, amount);
     }
     
-    function changeEtherDeltaDeposit(address newEthDelta) external {
-        if (msg.sender!=owner) throw;
-        ethDeltaDepositAddress = newEthDelta;
-        deltaDeposits = etherDelta(newEthDelta);
-    }
-    
+    // change owner this this trader wallet
     function changeOwner(address newOwner) external {
-        if (msg.sender!=owner) throw;
+        require(msg.sender==owner);
         owner = newOwner;
     }
     
+    // send ether to another wallet
+    function sendEther(address toAddress, uint amount) external {
+        require(msg.sender==owner);
+        toAddress.transfer(amount);
+    }
+    
+    // standard erc20 transfer/send function
+    function sendToken(address tokenAddress, address sendTo, uint256 amount) external {
+        require(msg.sender==owner);
+        Token token = Token(tokenAddress);
+        token.transfer(sendTo, amount);
+    }
+    
+    // let the owner execute with data
     function execute(address _to, uint _value, bytes _data) external returns (bytes32 _r) {
-        if (msg.sender!=owner) throw;
+        require(msg.sender==owner);
         require(_to.call.value(_value)(_data));
         return 0;
     }
     
+    // get ether delta token balance from token address
     function EtherDeltaTokenBalance(address tokenAddress) constant returns (uint) {
-        return deltaDeposits.balanceOf(tokenAddress, this);
+        return ethDelta.balanceOf(tokenAddress, this);
     }
     
+    // withdraw a token from etherdelta
     function EtherDeltaWithdrawToken(address tokenAddress, uint amount) payable external {
-        if (msg.sender!=owner) throw;
-        deltaDeposits.withdrawToken(tokenAddress, amount);
+        require(msg.sender==owner);
+        ethDelta.withdrawToken(tokenAddress, amount);
     }
     
+    // change etherdelta exchange address
+    function changeEtherDeltaDeposit(address newEthDelta) external {
+        require(msg.sender==owner);
+        ethDeltaDepositAddress = newEthDelta;
+        ethDelta = etherDelta(newEthDelta);
+    }
+    
+    // deposit tokens to etherdelta
     function EtherDeltaDepositToken(address tokenAddress, uint amount) payable external {
-        if (msg.sender!=owner) throw;
-        deltaDeposits.depositToken(tokenAddress, amount);
+        require(msg.sender==owner);
+        ethDelta.depositToken(tokenAddress, amount);
     }
     
+    // approve etherdelta to take take a specific amount
     function EtherDeltaApproveToken(address tokenAddress, uint amount) payable external {
-        if (msg.sender!=owner) throw;
+        require(msg.sender==owner);
         Token token = Token(tokenAddress);
         token.approve(ethDeltaDepositAddress, amount);
     }
     
+    // deposit ether to etherdelta
     function EtherDeltaDeposit(uint amount) payable external {
-        if (msg.sender!=owner) throw;
-        deltaDeposits.deposit.value(amount)();
+        require(msg.sender==owner);
+        ethDelta.deposit.value(amount)();
     }
     
+    // withdraw ether from etherdelta
     function EtherDeltaWithdraw(uint amount) external {
-        if (msg.sender!=owner) throw;
-        deltaDeposits.withdraw(amount);
+        require(msg.sender==owner);
+        ethDelta.withdraw(amount);
+    }
+    
+    // destroy this wallet and send all ether to sender
+    // THIS DOES NOT INCLUDE ERC20 TOKENS
+    function kill() {
+        require(msg.sender==owner);
+        suicide(msg.sender);
     }
     
 }
